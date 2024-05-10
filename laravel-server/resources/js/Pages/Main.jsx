ea3064b88fa7c0fe465d2a3486f0ad99ffc9ab57
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-10 09:49:23 +0300

    feat: add main page

---
+++ laravel-server/resources/js/Pages/Main.jsx
@@ -0,0 +1,28 @@
+import { Link, Head } from "@inertiajs/react";
+import img from "../../assets/logo.png";
+import GuestMenu from "./GuestMenu";
+import AuthenticatedMenu from "./AuthenticatedMenu";
+import Modal from "./Modal";
+
+export default function Main(auth) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className=" text-black/50 dark:bg-black dark:text-white/50">
+                <div className="relative  flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                        <main className="mt-12">
+                            {auth.auth.auth ? (
+                                <AuthenticatedMenu></AuthenticatedMenu>
+                            ) : (
+                                <GuestMenu></GuestMenu>
+                            )}
+                        </main>
+                        <footer className="py-16 text-center text-sm text-black dark:text-white/70"></footer>
+                    </div>
+                </div>
+            </div>
+            <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        </>
+    );
+}
