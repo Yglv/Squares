f80e6b0ea3f982b5d1da8326b4a68f41f43defc2
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-10 09:47:40 +0300

    feat: add main menu component for authenticated user

--- laravel-server/resources/js/Layouts/AuthenticatedLayout.jsx
+++ laravel-server/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,6 +4,7 @@ import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link } from "@inertiajs/react";
+import img from "../../assets/logo.png";
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
@@ -15,11 +16,7 @@ export default function Authenticated({ user, header, children }) {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
-                            </div>
+                            <div className="shrink-0 flex items-center"></div>
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
