f6fd39f7433749b27c166a7ef6b095c5c6731502
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-12 09:24:16 +0300

    fix: move the menu items to a separate component

--- laravel-server/resources/js/Pages/AuthenticatedMenu.jsx
+++ laravel-server/resources/js/Pages/AuthenticatedMenu.jsx
@@ -1,6 +1,7 @@
 import { Link } from "@inertiajs/react";
 import Modal from "./Modal";
 import { v1 as uuid } from "uuid";
+import Card from "./Card";
 
 export default function AuthenticatedMenu() {
     return (
@@ -11,22 +12,11 @@ export default function AuthenticatedMenu() {
                     id="docs-card"
                     className="flex flex-col group items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#14C7C3]  hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  lg:pb-32 lg:pt-32 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
-                    <div className="relative w-full flex items-center justify-center gap-6 ">
-                        <div
-                            id="docs-card-content"
-                            className="flex items-center gap-6 lg:flex-col"
-                        >
-                            <div className="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#14C7C3] sm:size-16">
-                                <i class="fa-solid fa-video text-white text-xl"></i>
-                            </div>
-
-                            <div className="pt-3 sm:pt-5 lg:pt-0">
-                                <h2 className="text-2xl font-semibold text-black dark:text-white group-hover:text-white">
-                                    Начать встречу
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                        title="Начать встречу"
+                        pin="fa-solid fa-video"
+                        background="bg-[#14C7C3]"
+                    />
                 </Link>
 
                 <Link
@@ -49,28 +39,22 @@ export default function AuthenticatedMenu() {
                             </div>
                         </div>
                     </div>
+                    <Card
+                        title="Подключиться"
+                        pin="fa-solid fa-plus"
+                        background="bg-[#FA650A]"
+                    />
                 </Link>
 
                 <Link
                     href="https://laravel-news.com"
                     className="flex flex-col items-start group gap-6 lg:pb-32 lg:pt-32 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300  hover:ring-black/40 hover:bg-[#FF216A] focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
-                    <div className="relative w-full flex items-center justify-center gap-6 ">
-                        <div
-                            id="docs-card-content"
-                            className="flex items-center gap-6 lg:flex-col"
-                        >
-                            <div className="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF216A] sm:size-16">
-                                <i class="fa-solid fa-calendar text-white text-xl"></i>
-                            </div>
-
-                            <div className="pt-3 sm:pt-5 lg:pt-0">
-                                <h2 className="text-2xl font-semibold text-black dark:text-white group-hover:text-white">
-                                    Запланировать встречу
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                        title="Запланировать встречу"
+                        pin="fa-solid fa-calendar"
+                        background="bg-[#FF216A]"
+                    />
                 </Link>
             </div>
         </>
--- laravel-server/resources/js/Pages/GuestMenu.jsx
+++ laravel-server/resources/js/Pages/GuestMenu.jsx
@@ -1,4 +1,5 @@
 import { Link } from "@inertiajs/react";
+import Card from "./Card";
 
 export default function GuestMenu() {
     return (
@@ -9,63 +10,30 @@ export default function GuestMenu() {
                     id="docs-card"
                     className="flex flex-col group items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#14C7C3]  hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  lg:pb-32 lg:pt-32 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
-                    <div className="relative w-full flex items-center justify-center gap-6 ">
-                        <div
-                            id="docs-card-content"
-                            className="flex items-center gap-6 lg:flex-col"
-                        >
-                            <div className="flex p-10 shrink-0 items-center justify-center rounded-full bg-[#14C7C3] sm:size-16">
-                                <i class="fa-solid fa-user text-white text-2xl"></i>
-                            </div>
-
-                            <div className="pt-3 sm:pt-5 lg:pt-0">
-                                <h2 className="text-2xl font-semibold text-black dark:text-white group-hover:text-white">
-                                    Войти
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                        title="Войти"
+                        pin="fa-solid fa-user"
+                        background="bg-[#14C7C3]"
+                    />
                 </Link>
 
                 <Link
                     href={route("register")}
                     className="flex flex-col group items-start gap-6 lg:pb-32 lg:pt-32 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#FA650A] hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
-                    <div className="relative w-full flex items-center justify-center gap-6 ">
-                        <div
-                            id="docs-card-content"
-                            className="flex items-center gap-6 lg:flex-col"
-                        >
-                            <div className="flex p-10 shrink-0 items-center justify-center rounded-full bg-[#FA650A] sm:size-16">
-                                <i class="fa-solid fa-user-plus text-white text-2xl"></i>
-                            </div>
-
-                            <div className="pt-8 sm:pt-5 lg:pt-0">
-                                <h2 className="text-2xl font-semibold text-black dark:text-white group-hover:text-white">
-                                    Зарегистрироваться
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                        title="Зарегистрироваться"
+                        pin="fa-solid fa-user-plus"
+                        background="bg-[#FA650A]"
+                    />
                 </Link>
 
                 <Link className="flex flex-col items-start group gap-6 lg:pb-32 lg:pt-32 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300  hover:ring-black/40 hover:bg-[#FF216A] focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                    <div className="relative w-full flex items-center justify-center gap-6 ">
-                        <div
-                            id="docs-card-content"
-                            className="flex items-center gap-6 lg:flex-col"
-                        >
-                            <div className="flex p-10 shrink-0 items-center justify-center rounded-full bg-[#FF216A] sm:size-16">
-                                <i class="fa-solid fa-plus text-white text-2xl"></i>
-                            </div>
-
-                            <div className="pt-3 sm:pt-5 lg:pt-0">
-                                <h2 className="text-2xl font-semibold text-black dark:text-white group-hover:text-white">
-                                    Подключиться
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
+                    <Card
+                        title="Подключиться"
+                        pin="fa-solid fa-plus"
+                        background="bg-[#FF216A]"
+                    />
                 </Link>
             </div>
         </>
