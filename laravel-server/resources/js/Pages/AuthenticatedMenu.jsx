bd29b5bfb80e3703546d28f756636f82a33f3b84
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-14 09:58:07 +0300

    fix: fixed bug with modal window opening

--- laravel-server/resources/js/Pages/AuthenticatedMenu.jsx
+++ laravel-server/resources/js/Pages/AuthenticatedMenu.jsx
@@ -22,7 +22,7 @@ export default function AuthenticatedMenu() {
                     />
                 </Link>
 
-                <Link
+                <div
                     onClick={() => setOpen(true)}
                     className="flex flex-col group items-start gap-6 lg:pb-32 lg:pt-32 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#FA650A] hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
@@ -31,7 +31,7 @@ export default function AuthenticatedMenu() {
                         pin="fa-solid fa-plus"
                         background="bg-[#FA650A]"
                     />
-                </Link>
+                </div>
 
                 <Link
                     href="https://laravel-news.com"
