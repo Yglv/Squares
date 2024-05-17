b1735a9a575b9b7de359bac903107d17b647d8fa
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-17 08:09:53 +0300

    feat: add exception handlers

--- laravel-server/resources/js/Services/VideoService.js
+++ laravel-server/resources/js/Services/VideoService.js
@@ -1,25 +1,36 @@
 export function handleToggleVideo(stream){
-    console.log(stream);
-    const videoTrack = stream
-        .getTracks()
-        .find((track) => track.kind === "video");
-    videoTrack.enabled = !videoTrack.enabled;
+    try {
+        console.log(stream);
+        const videoTrack = stream
+            .getTracks()
+            .find((track) => track.kind === "video");
+        videoTrack.enabled = !videoTrack.enabled;
+    } catch(err){
+        console.log(err)
+    }
+
 };
 
 export function handleToggleRemoteVideo (e, socket){
-    if (e.target.getAttribute("data-video") === "hide") {
-        e.target.setAttribute("data-video", "show");
-        socket.emit(
-            "hide remote cam",
-            e.target.getAttribute("data-id")
-        );
-    } else {
-        e.target.setAttribute("data-video", "hide");
-        socket.emit(
-            "show remote cam",
-            e.target.getAttribute("data-id")
-        );
+    try{
+        console.log('remote video');
+        if (e.target.getAttribute("data-video") === "hide") {
+            e.target.setAttribute("data-video", "show");
+            socket.emit(
+                "hide remote cam",
+                e.target.getAttribute("data-id")
+            );
+        } else {
+            e.target.setAttribute("data-video", "hide");
+            socket.emit(
+                "show remote cam",
+                e.target.getAttribute("data-id")
+            );
+        }
+    } catch {
+        console.log(err);
     }
+
 };
 
 export function hideVideo(stream) {
