bf15c7a1b8d12fbefc955dcaacbeefeedc4b2c61
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-17 08:06:10 +0300

    feat: add exception handlers

--- laravel-server/resources/js/Services/AudioService.js
+++ laravel-server/resources/js/Services/AudioService.js
@@ -1,26 +1,35 @@
 export function handleToggleAudio(stream) {
-    console.log(stream);
-    const audioTrack = stream
-        .getTracks()
-        .find((track) => track.kind === "audio");
-    audioTrack.enabled = !audioTrack.enabled;
+    try {
+        console.log(stream);
+        const audioTrack = stream
+            .getTracks()
+            .find((track) => track.kind === "audio");
+        audioTrack.enabled = !audioTrack.enabled;
+    } catch (err){
+        console.log(err);
+    }
+
 };
 
-
 export function handleToggleRemoteAudio (e, socket) {
-    if (e.target.getAttribute("data-audio") === "hide") {
-        e.target.setAttribute("data-audio", "show");
-        socket.emit(
-            "hide remote audio",
-            e.target.getAttribute("data-id")
-        );
-    } else {
-        e.target.setAttribute("data-audio", "hide");
-        socket.emit(
-            "show remote audio",
-            e.target.getAttribute("data-id")
-        );
+    try {
+        if (e.target.getAttribute("data-audio") === "hide") {
+            e.target.setAttribute("data-audio", "show");
+            socket.emit(
+                "hide remote audio",
+                e.target.getAttribute("data-id")
+            );
+        } else {
+            e.target.setAttribute("data-audio", "hide");
+            socket.emit(
+                "show remote audio",
+                e.target.getAttribute("data-id")
+            );
+        }
+    } catch (err){
+        console.log(err);
     }
+
 };
 
 export function hideAudio(stream) {
