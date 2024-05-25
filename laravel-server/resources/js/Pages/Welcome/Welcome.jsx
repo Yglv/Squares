50a3df59cabe16055b16958bbb912b380c417990
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-05-25 10:49:32 +0300

    fix: replace component

---
+++ laravel-server/resources/js/Pages/Welcome/Welcome.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import UserLayout from "../../Layouts/UserLayout";
+//import Modal from "./Modal";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document
+            .getElementById("screenshot-container")
+            ?.classList.add("!hidden");
+        document.getElementById("docs-card")?.classList.add("!row-span-1");
+        document
+            .getElementById("docs-card-content")
+            ?.classList.add("!flex-row");
+        document.getElementById("background")?.classList.add("!hidden");
+    };
+
+    const [open, setOpen] = useState(false);
+    const [roomId, setRoomId] = useState("");
+    const [name, setName] = useState("");
+
+    return <UserLayout auth={auth.user}></UserLayout>;
+}
