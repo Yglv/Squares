e9eb4e47770a7e58a93ecfa16fa5fb6827fbba44
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-01 10:40:24 +0300

    feat: add calendar list

--- laravel-server/resources/js/Pages/Meetup/MeetupMenu/MeetupMenu.jsx
+++ laravel-server/resources/js/Pages/Meetup/MeetupMenu/MeetupMenu.jsx
@@ -3,18 +3,23 @@ import MeetupList from "../MeetupList/MeetupList";
 import { Link } from "@inertiajs/react";
 import img from "../../../../assets/logo.png";
 import AddMeetupForm from "@/Components/Form/AddMeetupForm";
+import CalendarForm from "@/Components/Form/CalendarForm";
 import { useState } from "react";
 import Header from "@/Layouts/Header";
 
 export default function MeetupMenu({ meetups, auth }) {
     const [open, setOpen] = useState(false);
+    const [openCalendar, setOpenCalendar] = useState(false);
     return (
         <>
             <div className="flex flex-col justify-center">
                 <Header auth={auth} />
                 <div>
                     <div className="flex mt-10 mx-auto w-[50%] justify-between">
-                        <div className=" group cursor-pointer flex gap-6 w-[450px] h-[150px] p overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#00FF7F] hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                        <div
+                            onClick={() => setOpenCalendar(true)}
+                            className=" group cursor-pointer flex gap-6 w-[450px] h-[150px] p overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:bg-[#00FF7F] hover:ring-black/40 focus:outline-none focus-visible:ring-[#FF2D20]  dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                        >
                             <CustomButton
                                 title="Открыть календарь"
                                 pin="fa-regular fa-calendar-days"
@@ -49,6 +54,11 @@ export default function MeetupMenu({ meetups, auth }) {
                 </div>
             </div>
             <AddMeetupForm open={open} setOpen={setOpen} id={auth.user.id} />
+            <CalendarForm
+                open={openCalendar}
+                meetups={meetups}
+                setOpen={setOpenCalendar}
+            />
         </>
     );
 }
