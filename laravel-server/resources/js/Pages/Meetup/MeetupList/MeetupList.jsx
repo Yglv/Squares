92dfd4dfc32d9045eb60e88af6bcc2f8c29cbb8a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-14 09:44:42 +0300

    fix: added props to meetup card component

--- laravel-server/resources/js/Pages/Meetup/MeetupList/MeetupList.jsx
+++ laravel-server/resources/js/Pages/Meetup/MeetupList/MeetupList.jsx
@@ -10,7 +10,15 @@ export default function MeetupList(props) {
         <>
             <div className="grid grid-cols-2   gap-y-[20px] gap-x-[70px] mt-[30px]">
                 {props.meetups.map((meetup) => {
-                    return <MeetupCard key={meetup.title} data={meetup} />;
+                    return (
+                        <MeetupCard
+                            key={meetup.title}
+                            icon="text-2xl"
+                            text="text-[34px]"
+                            mt="mt-[-5px]"
+                            data={meetup}
+                        />
+                    );
                 })}
             </div>
             ;
