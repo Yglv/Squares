564d51901af8b693dece6816d3793d718e72b7e0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-14 09:45:35 +0300

    fix: replaced meetups list in another menu

--- laravel-server/resources/js/Pages/MeetupCalendar/MeetupCalendar.jsx
+++ laravel-server/resources/js/Pages/MeetupCalendar/MeetupCalendar.jsx
@@ -6,13 +6,18 @@ import {
     isSameDay,
     isToday,
     startOfMonth,
+    add,
 } from "date-fns";
 import clsx from "clsx";
+import MeetupCard from "../Meetup/MeetupCard/MeetupCard";
+import { useState, useEffect } from "react";
 
 const weekdays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
 export default function MeetupCalendar({ meetups }) {
-    const currentDate = new Date();
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentMeetups, setCurrentMeetups] = useState([]);
+    const [selectedDay, setSelectedDay] = useState();
     const firstDayOfMonth = startOfMonth(currentDate);
     const lastDayOfMonth = endOfMonth(currentDate);
 
@@ -41,57 +46,101 @@ export default function MeetupCalendar({ meetups }) {
         return months[date.getMonth()];
     };
 
+    const getPreviousMonth = () => {
+        let firstDayNextMonth = add(currentDate, { months: -1 });
+        setCurrentDate(firstDayNextMonth);
+    };
+
+    const getNextMonth = () => {
+        let firstDayNextMonth = add(currentDate, { months: 1 });
+        setCurrentDate(firstDayNextMonth);
+    };
+
+    useEffect(() => {
+        setCurrentMeetups(
+            meetups.filter((meetup) =>
+                isSameDay(meetup.future_meetup_date, selectedDay)
+            )
+        );
+    }, [selectedDay]);
+
     return (
         <>
-            <div className="container mx-auto p-4">
-                <div className="mb-4">
-                    <p
-                        className="text-center font-bold text-2
-                    xl pb-8"
-                    >
-                        {format(currentDate, getMonthName(currentDate))}
-                    </p>
-                </div>
-                <div className="grid grid-cols-7 gap-2">
-                    {weekdays.map((day) => (
-                        <div
-                            className="font-bold text-lg text-center"
-                            key={day}
-                        >
-                            {day}
+            <div className="flex">
+                <div className="container h-[700px] w-[60%] flex-start p-8">
+                    <div className="flex mb-4 mx-auto my-0 w-[90%] justify-between">
+                        <p className="text-center font-bold text-4xl pb-8">
+                            {format(currentDate, getMonthName(currentDate))}
+                        </p>
+                        <div className="flex w-[8%] justify-between">
+                            <button onClick={getPreviousMonth}>
+                                <i class="fa-solid fa-circle-left text-4xl cursor-pointer"></i>
+                            </button>
+                            <button onClick={getNextMonth}>
+                                <i class="fa-solid fa-circle-right text-4xl cursor-pointer"></i>
+                            </button>
                         </div>
-                    ))}
-                    {Array.from({ length: startingDayIndex }).map(
-                        (_, index) => (
-                            <div key={`empty - ${index}`}></div>
-                        )
-                    )}
-                    {daysInMonth.map((day, index) => (
-                        <div
-                            key={index}
-                            className={clsx(
-                                "text-center text-lg  h-[80px] rounded-md hover:bg-blue-500 hover:text-white cursor-pointer",
-                                {
-                                    "bg-blue-500": isToday(day),
-                                    "text-white": isToday(day),
-                                }
-                            )}
-                        >
-                            {format(day, "d")}
-                            {meetups
-                                .filter((meetup) =>
+                    </div>
+                    <div className="grid grid-cols-7 gap-3">
+                        {weekdays.map((day) => (
+                            <div
+                                className="font-bold text-lg text-center"
+                                key={day}
+                            >
+                                {day}
+                            </div>
+                        ))}
+                        {Array.from({ length: startingDayIndex }).map(
+                            (_, index) => (
+                                <div key={`empty - ${index}`}></div>
+                            )
+                        )}
+                        {daysInMonth.map((day, index) => (
+                            <div
+                                key={index}
+                                onClick={(event) => {
+                                    event.stopPropagation;
+                                    console.log(format(day, "yyyy-MM-dd"));
+                                    setSelectedDay(format(day, "yyyy-MM-dd"));
+                                }}
+                                className={clsx(
+                                    "text-center text-lg mb-4 h-[80px] rounded-md hover:bg-blue-500 hover:text-white cursor-pointer",
+                                    {
+                                        "bg-blue-500": isToday(day),
+                                        "text-white": isToday(day),
+                                    }
+                                )}
+                            >
+                                {format(day, "d")}
+                                {meetups.filter((meetup) =>
                                     isSameDay(meetup.future_meetup_date, day)
-                                )
-                                .map((meetup) => (
-                                    <div
-                                        className="bg-green-400 rounded-xl text-white"
-                                        key={meetup.future_meetup_title}
-                                    >
-                                        {meetup.future_meetup_title}
-                                    </div>
-                                ))}
-                        </div>
-                    ))}
+                                ).length !== 0 && (
+                                    <div className="w-[20px] h-[20px] rounded-[100%] bg-green-500 mx-auto my-0"></div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="mt-12 md:mt-0 md:pl-14">
+                    <h2 className="font-bold text-3xl pt-8  text-gray-900">
+                        Запланированные встречи {selectedDay}
+                    </h2>
+                    <div className="mt-4 h-[600px] space-y-1 text-sm leading-6 overflow-y-auto text-gray-500">
+                        {currentMeetups.length > 0 ? (
+                            currentMeetups.map((meetup) => (
+                                <MeetupCard
+                                    data={meetup}
+                                    width="w=[150px] h-[50px]"
+                                    icon="text-2xl"
+                                    text="text-[30px]"
+                                    mt="mt-[5px]"
+                                    key={meetup.title}
+                                />
+                            ))
+                        ) : (
+                            <p>Запланированных встреч нет</p>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
